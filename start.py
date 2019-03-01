bc0b8af5918d7b59dc0af26230f41845fef802b9
Joshua Pabilona <joshua.pabilona@ivantageapps.com>
2019-03-01 10:21:39 +0800

    finish quiz app for math

--- start.py
+++ start.py
@@ -1,12 +1,13 @@
+# TODO: main program body looping -- exit when an exit state is reached
 # TODO: add score and ASCII arcade start screen
 # TODO: look up module and package organization
 # TODO: look up if basename imports like these first ones chuck everything
+import csv
 import os
 import random
-import re
 
 from contextlib import ExitStack
-from enum import Enum
+from enum import Enum, unique
 from itertools import zip_longest
 from pick import pick
 
@@ -16,10 +17,13 @@ class Question(object):
     # TODO: make attributes self-documenting
     # TODO: where to force expectation, caller or handler
     def __init__(self, question, choices, answer, difficulty, chosen=None):
+        # question = also known as the stem
         self.question = question
         self.choices = choices
+        # answer = also known as a keyed option
         self.answer = answer
         self.difficulty = difficulty
+        # rename this to answer
         self.chosen = chosen
     
     def __repr__(self):
@@ -31,9 +35,10 @@ class Question(object):
     
     def prompt(self):
         return f'{self.question} [{self.difficulty.name}]'
-    
 
+@unique
 class Difficulty(Enum):
+    __order__ = "EASY MEDIUM HARD"
     EASY = 1
     MEDIUM = 2
     HARD = 3
@@ -42,15 +47,17 @@ CATEGORIES = {'Mathematics':'math', 'Science':'science',
               'Philippine History':'phil'}
 
 def choose_category():
+    """Returns the value of the chosen category as a string."""
     main_prompt = 'Please choose your desired quiz category:'
     categories = list(CATEGORIES.keys())
     chosen = _pick(prompt=main_prompt, options=categories)
     return CATEGORIES[chosen]
 
 def start_quiz():
-    """Returns a list of questions and the user's responses."""
+    """Returns a list of answered Question objects."""
     category = choose_category()
     questions = generate_questions(category)
+    # Answering part
     for question in questions:
         chosen = _pick(prompt=question.prompt(), options=question.choices)
         question.chosen = chosen
@@ -67,19 +74,29 @@ def generate_questions(category):
     # given the path
     # we want to return a list of randomized Question objects
     questions = []
+    filenames_by_difficulty = dict()
+    # {<Difficulty.EASY: 1>:<easy_filenames>, 
+    #  <Difficulty.MEDIUM: 2>:<medium_filenames} ... }
     category_dir = os.path.abspath(category)
-    # automate searching for difficulty
-    filenames = [f for f in os.listdir(category_dir) if f.endswith('_easy.txt')]
+    for d in Difficulty:
+        filenames_by_difficulty[d] = [os.path.join(category_dir, f) 
+                                      for f in os.listdir(category_dir)
+                                      if d.name.lower() in f]
+    print(filenames_by_difficulty)
+
     with ExitStack() as stack:
-        files = [stack.enter_context(open(os.path.join(category_dir, fname)))
-                 for fname in filenames]
-        for answer, choices, question in zip_longest(*files):
-            choices = choices.strip().split(" ")
-            answer = answer.strip()
-            questions.append(Question(question=question,
-                                      choices=choices,
-                                      answer=answer,
-                                      difficulty=Difficulty.EASY))
+        for difficulty, fnames in filenames_by_difficulty.items():
+            files = [stack.enter_context(open(fname)) for fname in fnames]
+            print(files)
+            for answer, choices, question in zip_longest(*files):
+                # Read choice csv
+                choices = choices.strip().split(" ")
+                answer = answer.strip()
+                question = question.strip()
+                questions.append(Question(question=question,
+                                        choices=choices,
+                                        answer=answer,
+                                        difficulty=difficulty))
     random.shuffle(questions)
     return questions
 
